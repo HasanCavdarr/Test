9f08232b7655bf85cdab83d626c64d76d91b2da9
Hasan <72728950+HasanCavdarr@users.noreply.github.com>
2024-05-12 00:19:46 +0300

    Delete deneme1.py

--- deneme1.py
+++
@@ -1,6 +0,0 @@
-def calculate_average(a,b):
-    return (a+b)/2
-number1 = int(input("first number is: "))
-number2 = int(input("second number is: "))
-answer = calculate_average(number1,number2)
-print("answer: ",answer)
