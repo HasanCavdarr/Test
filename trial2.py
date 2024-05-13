7e8b55bb5e539e7cb4f84ddcf31732facb3bf4f6
Hasan <hasan_cavdar2017@outlook.com>
2024-05-14 01:00:29 +0300

    feat commit is created

--- trial2.py
+++ trial2.py
@@ -7,3 +7,11 @@ numbers = [15, 25, 35, 45, 55]
 
 result = sum_of_list(numbers)
 print("sum of the list:", result)
+
+def main(a):
+    if a % 2 == 0:
+        print("your number is even")
+    else: 
+        print("your number is odd")
+number3 = int(input("enter a number"))
+answer = main(number3)
